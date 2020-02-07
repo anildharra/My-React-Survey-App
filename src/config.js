1000afe499b5a3ea694bb6d8ab2639f76a2bef62
anildharra <anilishu2006@gmail.com>
2020-02-08 04:28:00 +0530

    initial commit

---
+++ src/config.js
@@ -0,0 +1,15 @@
+
+ const firebaseConfig = {
+    apiKey: "AIzaSyAkTPnfqY6T-LEL65DW_Ir6Ptz92wGMbQQ",
+    authDomain: "usurvey-d30b4.firebaseapp.com",
+    databaseURL: "https://usurvey-d30b4.firebaseio.com",
+    projectId: "usurvey-d30b4",
+    storageBucket: "usurvey-d30b4.appspot.com",
+    messagingSenderId: "899017004689",
+    appId: "1:899017004689:web:b51e937c52179117209130",
+    measurementId: "G-7K1MLVG3N8"
+  };
+
+  export default firebaseConfig;
+  
+  
